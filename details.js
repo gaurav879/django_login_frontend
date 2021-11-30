d3da5c5d1f60033fa5e2c04f41508e33c26a3c99
gaurav879 <gaurav91306@gmail.com>
2021-11-30 19:52:49 +0530

    implemented delete and logout feature

--- details.js
+++ details.js
@@ -2,6 +2,9 @@ let userId = document.querySelector("#t_username");
 let _name = document.querySelector("#t_name");
 let email = document.querySelector("#t_email");
 let address = document.querySelector("#t_address");
+let logout = document.querySelector("#logout");
+let _delete =document.querySelector("#delete");
+
 
 async function l_handleSubmit() {
     let x = window.localStorage.getItem('jwt')
@@ -29,7 +32,40 @@ async function l_handleSubmit() {
     console.log(res);
 
   }
+
+async function handleSubmit() {
+    document.cookie = 'jwt='
+    localStorage.setItem("jwt","")
+    const res = await fetch("http://localhost:8000/api/logout",{
+      method : "GET"
+    })
+    .then(function(res)
+    {
+        window.location.replace("http://localhost:5500/index.html")
+    })
+}
+async function d_handleSubmit() {
+    // let x = window.localStorage.getItem('jwt')
+    // console.log(window.localStorage.getItem('jwt'))
+    // console.log(x)
+    data = {
+        jwt : window.localStorage.getItem('jwt')
+    }
+    const res = await fetch("http://localhost:8000/api/user",{
+      method : "DELETE",
+      body : JSON.stringify(data)
+    })
+    .then(function(res)
+    {
+        window.location.replace("http://localhost:5500/index.html")
+    })
+}
+
 l_handleSubmit()
+logout.addEventListener("click", handleSubmit);
+_delete.addEventListener("click", d_handleSubmit);
+
+
 
 
 
